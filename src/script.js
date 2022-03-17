a6ed39a3121bc31cd097fc45d67f2165e481020d
MelinaDevAdmin <mely.24a@gmail.com>
2022-03-17 10:29:15 +0100

    Commit 2
    Forecast, current location,color changes

--- src/script.js
+++ src/script.js
@@ -38,6 +38,60 @@ function formatDay(timestamp) {
     return days[day];
 }
 
+function displayForecast(response) {
+    let forecast = response.data.daily;
+
+    let forecastElement = document.querySelector("#forecast");
+
+    let forecastHTML = `<div class="row">`;
+    forecast.forEach(function (forecastDay, index) {
+        if (index < 6) {
+            forecastHTML = forecastHTML + `
+      <div class="col-2">
+        <div class="weather-forecast-date">${
+                formatDay(forecastDay.dt)
+            }</div>
+        
+        <img 
+        src="http://openweathermap.org/img/wn/${
+                forecastDay.weather[0].icon
+            }@2x.png" 
+        alt="" 
+        width="42"
+    />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-max"> ${
+                Math.round(forecastDay.temp.max)
+            }° </span>
+          <span 
+          class="weather-forecast-min"> ${
+                Math.round(forecastDay.temp.min)
+            }° </
+          span>
+        </div>
+      </div>
+  `;
+        }
+    });
+
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+    console.log(forecastHTML);
+}
+
+function getForecast(coordinates) {
+    console.log(coordinates);
+    let apiKey = "e4799330ffe003d2d7f69849dc03f789";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${
+        coordinates.lat
+    }&lon=${
+        coordinates.lon
+    }&appid=${apiKey}&units=imperial`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(displayForecast);
+}
+
+
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
@@ -64,6 +118,8 @@ function displayTemperature(response) {
     }@2x.png`);
     iconElement.setAttribute("alt", response.data.weather[0].description);
     console.log(response);
+    getForecast(response.data.coord);
+
 
 }
 
@@ -81,6 +137,37 @@ function signUp(event) {
     search(searchinput.value);
 
 }
+
+function searchLocation(position) {
+    let apiKey = "7432bc33052157d23671be783f19aba7";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${
+        position.coords.latitude
+    }&lon=${
+        position.coords.longitude
+    }&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(displayTemperature);
+}
+
+
+function showPosition(position) {
+
+    console.log(`Your Latitude is ${
+        position.coords.latitude
+    } and your longitude is ${
+        position.coords.longitude
+    }`);
+    searchLocation(position);
+
+}
+
+function getCurrentLocation() {
+
+    navigator.geolocation.getCurrentPosition(showPosition);
+
+}
+search("The Hague");
 let form = document.querySelector("#searchform");
 form.addEventListener("submit", signUp);
-search("The Hague");
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
